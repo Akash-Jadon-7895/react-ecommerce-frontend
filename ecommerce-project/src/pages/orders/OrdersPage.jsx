60fe8bafae8ca60ec9dbcb5786b3191a6b3a8fb8
Akash <jadonakash672@gmail.com>
2026-01-15 16:06:33 +0530

    Extract DeliveryDate and cart item components

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { useState, useEffect, Fragment } from 'react';
 import dayjs from 'dayjs';
-import { Header } from '../components/Header';
+import { Header } from '../../components/Header';
 import { Link } from 'react-router';
 import BuyAgainIcon from '../assets/images/icons/buy-again.png';
-import { formatMoney } from '../utils/money';
+import { formatMoney } from '../../utils/money';
 import './OrdersPage.css';
 
 
