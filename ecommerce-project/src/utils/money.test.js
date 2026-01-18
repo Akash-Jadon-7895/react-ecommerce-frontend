28c68fc31ce41997858b073a1af6bd1f91d90860
Akash <jadonakash672@gmail.com>
2026-01-18 23:23:34 +0530

    Add zero-value test case for formatMoney

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -11,4 +11,9 @@ describe('formatMoney', () => {
     expect(formatMoney(1090)).toBe('$10.90');
     expect(formatMoney(100)).toBe('$1.00');
   });
+
+  it('formats 0 cents as $0.00', () => {
+    expect(formatMoney(0)).toBe('$0.00');
+  });
+
 });
