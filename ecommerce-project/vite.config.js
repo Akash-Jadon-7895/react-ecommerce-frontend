dd9927ddfadb3252dc0b5296f37fbfd0a3fdddf6
Akash <jadonakash672@gmail.com>
2026-01-25 03:13:26 +0530

    Set Up React Compiler

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3000'
       },
-      '/images':{
+      '/images': {
         target: 'http://localhost:3000'
       }
     }
